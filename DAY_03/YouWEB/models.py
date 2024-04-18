8fff383e85a8014a862f5627ff0d831effebcdc1
Schubert3275 <dragon3275@gmail.com>
2024-04-18 17:43:23 +0900

    웹 활용 3일차

--- DAY_03/YouWEB/models.py
+++ DAY_03/YouWEB/models.py
@@ -5,7 +5,7 @@ from YouWEB import db
 ## Question 테이블 클래스 -------------------------------------
 ## 컬럼 : id, subject, content, create_date
 class Question(db.Model):
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    id = db.Column(db.Integer, primary_key=True)
     subject = db.Column(db.String(200), nullable=False)
     content = db.Column(db.Text(), nullable=False)
     create_date = db.Column(db.DateTime(), nullable=False)
